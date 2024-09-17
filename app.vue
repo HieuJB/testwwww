7b365ab392e90f3d3ac805aabbe26767e6eaeda2
justin <justin@sclbo.com>
2024-09-18 00:06:00 +0800

    feat: bug

--- app.vue
+++ app.vue
@@ -8,7 +8,7 @@
 if (process.client && 'serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js').then(function(reg) {
     console.log('Service Worker Registered');
-  });dsadsa
+  });
 }
 </script>
 
