5ced9bf9deceb7eba448a0e13d249ca2dca28408
justin <justin@sclbo.com>
2024-09-18 00:04:25 +0800

    feat: bug

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,32 @@
+import { VitePWA } from 'vite-plugin-pwa'
+
 export default defineNuxtConfig({
-  buildModules: [
-    '@nuxtjs/pwa',
-  ],
+  modules: ['@vite-pwa/nuxt'],
   pwa: {
     manifest: {
       name: 'My PWA App',
       short_name: 'PWA',
       theme_color: '#ffffff',
+      background_color: '#ffffff',
+      display: 'standalone',
+      start_url: '/',
+      icons: [
+        {
+          src: '/icon-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: '/icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
     },
     workbox: {
-      enabled: true, // Bật workbox
-    },
-  },
+      cleanupOutdatedCaches: true, // Dọn dẹp cache cũ
+      clientsClaim: true, // Lấy quyền kiểm soát tất cả các tab
+      skipWaiting: true // Kích hoạt ngay lập tức khi Service Worker mới được cài đặt
+    }
+  }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -9,6 +9,8 @@ onMounted(() => {
   if (process.client && 'serviceWorker' in navigator) {
     navigator.serviceWorker.ready.then(function (reg) {
       setTimeout(() => {
+        console.log('============');
+        
         reg.showNotification('Thông báo', {
           body: 'Thời gian hiện tại: ' + new Date().toLocaleTimeString(),
           icon: '/icon.png',
