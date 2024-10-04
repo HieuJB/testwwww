17a0861842c182f953cf0bfcfd4a5f51fce86a0e
justin <justin@sclbo.com>
2024-10-04 22:54:48 +0800

    feat: bug

---
+++ app.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    {{ data1 }}
+  </div>
+</template>
+
+<script setup>
+const data1 = ref('')
+
+const fetchData = async () => {
+  const { data } = await useFetch('https://trunghieuit.click/api/time')
+  data1.value = data.value.time
+  
+}
+
+await fetchData()
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
