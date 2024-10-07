337b25c9c441699783e121d14a93dca11f73b529
justin <justin@sclbo.com>
2024-10-07 10:07:44 +0800

    feat: update code

--- .output/public/_nuxt/BaIxpP7C.js
+++
@@ -1,1 +0,0 @@
-import{aN as e,c as a,o}from"./BCYEZECI.js";const s={__name:"[category]",setup(t){return e(!1),(r,c)=>(o(),a("h1",null,"Football"))}};export{s as default};
--- .output/server/chunks/build/_match_id_-styles.C27P303Q.mjs
+++
@@ -1,6 +0,0 @@
-const _match_id__vue_vue_type_style_index_0_scoped_e7d7dbd8_lang = ".rankbox[data-v-e7d7dbd8]{.eTable tr[data-v-e7d7dbd8]{th[data-v-e7d7dbd8]{height:40px}td[data-v-e7d7dbd8]{height:40px!important}}.rankdata[data-v-e7d7dbd8]{.eTable tr[data-v-e7d7dbd8]{th[data-v-e7d7dbd8]{height:40px;width:110px}td[data-v-e7d7dbd8]{height:40px!important}}}}";
-
-const _match_id_Styles_C27P303Q = [_match_id__vue_vue_type_style_index_0_scoped_e7d7dbd8_lang];
-
-export { _match_id_Styles_C27P303Q as default };
-//# sourceMappingURL=_match_id_-styles.C27P303Q.mjs.map
